55b60333e9adda04a51ec66a24443135934b90c0
Elie Dutheil <eliedthl@gmail.com>
2016-09-29 17:17:08 +0200

    Modification du manifest pour accepter l'api Trello, ajout de jQuery et mise en place d'une fonction de login qui se déclenche au clic du bouton

---
+++ hw-extension/helloworld.js
@@ -0,0 +1,9 @@
+function login(){
+  // Login on Trello is done here
+
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('button').addEventListener('click', login);
+  main();
+});
