0d585a0bcf1690881fc772aaae50f0fa54c88a85
Elie Dutheil <eliedthl@gmail.com>
2016-10-06 16:17:05 +0200

    Merge of : - button add on seLoger - Scrapping to get picture of the offer - Authentication Trello + creation of a new board

--- trelloGementExtension/scripts/inject.js
+++ trelloGementExtension/scripts/inject.js
@@ -2,9 +2,13 @@ var buttonLocalisation = $(".resume__infos .resume__action");
 var button = $("<button>", {
   type: "button",
   id:"button_trello",
+  onclick:"scrap()",
   text:"Ajouter à Trellogement",
   alt:"Ajouter à Trellogement",
 })
 
-
 buttonLocalisation.append(button);
+
+var s = document.createElement('script');
+s.src = chrome.extension.getURL('scripts/scrapping.js');
+(document.head).appendChild(s);
--- trelloGementExtension/scripts/scrapping.js
+++ trelloGementExtension/scripts/scrapping.js
@@ -1,19 +1,5 @@
 function scrap() {
   // scrap datas from the current url
-  chrome.tabs.getSelected(null,function(tab) {
-    var url = tab.url;
-
-    $.get(url, function (data) {
-      $page = $(data);
-      var $img = $('img.carrousel_image_visu', $page).attr('src');
-
-      console.log($img);
-
-      $('#results').append('<img src="' + $img + '">');
-    });
-  });
+    var $img = $('img.carrousel_image_visu').attr('src');
+    alert($img);
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('button').addEventListener('click', scrap);
-});
