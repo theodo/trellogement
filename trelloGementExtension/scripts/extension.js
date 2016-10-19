d11b6044239987096cff9e2d7d952a091c5955cf
paulden <paul.de-nonancourt@student.ecp.fr>
2016-10-19 18:33:26 +0200

    Added compare function to determine which button should be implemented in seLoger.com page

--- trelloGementExtension/scripts/extension.js
+++ trelloGementExtension/scripts/extension.js
@@ -1,3 +1,16 @@
+function scrap() {
+  // scrap datas from the current url and return an dictionnary
+    var infos = {
+      img: $('img.carrousel_image_visu').attr('src'),
+      price: $('span.resume__prix').text().trim(),
+      title: $('h1.detail-title').text().replace(/\r?\n|\r/g,'').replace(/ +(?= )/g,'').trim(),
+      description: $('p.description').html(),
+      link: window.location.href,
+    };
+
+    return infos;
+}
+
 // Prepare Trello client
 chrome.storage.local.get('trellogement_trello_key', function (key) {
   Trello.setKey(key.trellogement_trello_key);
@@ -11,6 +24,26 @@ chrome.storage.local.get('trellogement_trello_token', function (token) {
 chrome.storage.local.get('trellogement_trello_board_id', function (boardId) {
   var boardId = boardId.trellogement_trello_board_id;
   init(boardId);
+  compare(boardId);
+});
+
+//Creating Trello buttons and button localisation
+
+var buttonLocalisation = $(".resume__infos .resume__action");
+
+var buttonAdded = $("<button>", {
+  type: "button",
+  id:"button_trello",
+  text:"Déjà ajouté !",
+  alt:"Déjà ajouté !",
+});
+
+var buttonToAdd = $("<button>", {
+  type: "button",
+  id:"button_trello",
+  onclick:"scrap()",
+  text:"Ajouter à Trellogement",
+  alt:"Ajouter à Trellogement",
 });
 
 // Init function
@@ -22,6 +55,29 @@ function init(boardId) {
   });
 }
 
-var s = document.createElement('script');
-s.src = chrome.extension.getURL('scripts/inject.js');
-(document.head).appendChild(s);
+//Compare function
+function compare(boardId){
+  var currentPage = scrap();
+  console.log(currentPage);
+  var checkIfCardPresent = false;
+  var idCard = null;
+  Trello.get(`/boards/${boardId}/cards`, function(cards) {
+    for (card of cards) {
+      if (card.name == currentPage['title']) {
+        checkIfCardPresent = true;
+        var idCard = card.id
+        break;
+        }
+      }
+
+      // Appending the correct button :
+      //if the ad has already been added
+      if (checkIfCardPresent) {
+        buttonLocalisation.append(buttonAdded);
+      } else { //if the ad hasn't been added
+        buttonLocalisation.append(buttonToAdd);
+      }
+
+      console.log(checkIfCardPresent);
+    });
+}
--- trelloGementExtension/scripts/inject.js
+++ trelloGementExtension/scripts/inject.js
@@ -1,14 +1,3 @@
-var buttonLocalisation = $(".resume__infos .resume__action");
-var button = $("<button>", {
-  type: "button",
-  id:"button_trello",
-  onclick:"scrap()",
-  text:"Ajouter à Trellogement",
-  alt:"Ajouter à Trellogement",
-});
-
-buttonLocalisation.append(button);
-
 function scrap() {
   // scrap datas from the current url and return an dictionnary
     var infos = {
