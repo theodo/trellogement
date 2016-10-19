dfad0cd9ac18ccfa45d4acf0e4dbdef3d09b769d
Elie Dutheil <eliedthl@gmail.com>
2016-10-19 18:08:53 +0200

    Add eventHandler watching "Ajouter à mon board" button and creating onclick the new card in the "Je suis interressé" list which contains title, image, price, description and link of the offer on se loger.com

--- trelloGementExtension/scripts/extension.js
+++ trelloGementExtension/scripts/extension.js
@@ -1,3 +1,15 @@
+// scrap datas from the current url and return an dictionnary
+function scrap() {
+    var infos = {
+      img: $('img.carrousel_image_visu').attr('src'),
+      price: $('span.resume__prix').text().trim(),
+      title: $('h1.detail-title').text().replace(/\r?\n|\r/g,'').replace(/ +(?= )/g,'').trim(),
+      description: $('p.description').html(),
+      link: window.location.href,
+    };
+    return infos;
+}
+
 // Prepare Trello client
 chrome.storage.local.get('trellogement_trello_key', function (key) {
   Trello.setKey(key.trellogement_trello_key);
@@ -11,6 +23,44 @@ chrome.storage.local.get('trellogement_trello_token', function (token) {
 chrome.storage.local.get('trellogement_trello_board_id', function (boardId) {
   var boardId = boardId.trellogement_trello_board_id;
   init(boardId);
+
+  // This part handles request made from seLoger pages by clicking on buttons
+  window.addEventListener("message", function(event) {
+    // We only accept messages from the page
+    if (event.source != window)
+      return;
+
+    // Action to create a new card
+    if (event.data == 'createCard') {
+
+      var infos = scrap();
+      Trello.get('/boards/' + boardId + '/lists', { fields: "id,name" }, function(lists) {
+        // Getting list id
+        var listId;
+        for (index in lists)
+        {
+          if (lists[index].name == "Je suis intéressé")
+          {
+            listId = lists[index].id;
+          }
+        }
+
+        // Creating the new card
+        var newCard = {
+          name: infos['title'],
+          desc: 'Prix du logement : ' + infos['price'] + '\n\n Lien vers la page : ' + infos['link'],
+          urlSource: infos['img'],
+          idList: listId,
+          pos: 'top'
+        };
+        Trello.post('/cards/', newCard, function(data) {
+          // Creating here the comment with description of offer in it
+          Trello.post('/cards/' + data.id + '/actions/comments', { text : scrap()['description']});
+          console.log('Card created successfully.');//TODO change button on the page
+        });
+      });
+    }
+  }, false);
 });
 
 // Init function
--- trelloGementExtension/scripts/inject.js
+++ trelloGementExtension/scripts/inject.js
@@ -2,22 +2,17 @@ var buttonLocalisation = $(".resume__infos .resume__action");
 var button = $("<button>", {
   type: "button",
   id:"button_trello",
-  onclick:"scrap()",
+  onclick:"createCard()",
   text:"Ajouter à Trellogement",
   alt:"Ajouter à Trellogement",
 });
 
 buttonLocalisation.append(button);
 
-function scrap() {
-  // scrap datas from the current url and return an dictionnary
-    var infos = {
-      img: $('img.carrousel_image_visu').attr('src'),
-      price: $('span.resume__prix').text().trim(),
-      title: $('h1.detail-title').text().replace(/\r?\n|\r/g,'').replace(/ +(?= )/g,'').trim(),
-      description: $('p.description').html(),
-      link: window.location.href,
-    };
-
-    return infos;
+function createCard() {
+    /*
+     * A message containing the action is posted on the page itself on click
+     * and captured by the extension to process datas.
+    */
+    window.postMessage("createCard", "*");
 }
