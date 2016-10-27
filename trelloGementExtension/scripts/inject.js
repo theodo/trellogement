f5382c50516af73836e70c380e730e31d0f9757a
Elie Dutheil <eliedthl@gmail.com>
2016-10-27 12:36:08 +0200

    Add prop disabled onclick of trello button

--- trelloGementExtension/scripts/inject.js
+++ trelloGementExtension/scripts/inject.js
@@ -1,4 +1,5 @@
 function createCard() {
+    $('#button_trello').prop("disabled",true);
     /*
      * A message containing the action is posted on the page itself on click
      * and captured by the extension to process datas.
