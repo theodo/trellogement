98a7046226ce4273444485290682e803eaad93e5
Elie Dutheil <eliedthl@gmail.com>
2016-10-27 12:09:41 +0200

    Cursor pointer on troll button

--- trelloGementExtension/scripts/inject.js
+++ trelloGementExtension/scripts/inject.js
@@ -3,5 +3,5 @@ function createCard() {
      * A message containing the action is posted on the page itself on click
      * and captured by the extension to process datas.
     */
-    window.postMessage("createCard", "*");
+    window.postMessage('createCard', '*');
 }
